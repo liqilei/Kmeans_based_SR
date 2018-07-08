7dabf68f0d5e85648504c85957a4dbe293375b79
Paper99 <357309275@qq.com>
2018-07-08 16:43:22 +0800

    Update VDSR archs

--- README.md
+++ README.md
@@ -6,14 +6,18 @@ Code structure is inspired by [pytorch-cyclegan](https://github.com/junyanz/pyto
 - `num_workers` in **options/train/train.json** should fix at 1. If you want to use multiple `num_workers`. You should firstly install **hdf5 package with parallel settings**. You can find solutions from [here](http://docs.h5py.org/en/latest/build.html#building-against-parallel-hdf5)
 
 ## Running now
-- SRCNN_K: x3: C1 C2
 
 ## TODO
-- SRCNN_K: x3: C3 C4 C5 C6; x2: Best; x4: Best.
-- VDSR_K: x3: C1 C2 C3 C4 C5 C6; x2: Best; x4: Best
+### Zhen Li
+Deadline: 7.9
+- VDSR : complete baseline 
+- Prepare data for VDSR
+- run VDSR
+
+### Qilei Li
 
 ## Requirements
-- Python3
+- Python 3.6
 - Pytorch
 - MatConvNet(It is included in `scripts\Test`, you should complie it firstly)
 
---
+++ models/modules/vdsr.py
@@ -0,0 +1,59 @@
+import torch.nn as nn
+import torch
+
+import models.modules.blocks as B
+
+
+class vdsr(nn.Module):
+    def __init__(self, in_channels, out_channels, num_branch):
+        super(vdsr, self).__init__()
+        self.conv = nn.ModuleList()
+
+        self.conv_in = B.ConvBlock(in_channels, 64, kernel_size=3, norm_type=None, act_type='relu',valid_padding=True)
+
+        for i in range(18):
+            self.conv.append(B.ConvBlock(64, 64, kernel_size=3, norm_type=None, act_type='relu',valid_padding=True))
+
+        self.conv_out = B.ConvBlock(64, out_channels, kernel_size=3, norm_type=None, act_type=None, valid_padding=True)
+
+    def forward(self, input):
+        x = self.conv_in(input)
+
+        for c in self.conv:
+            x = c(x)
+
+        output = torch.add(self.conv_out(x), input)
+
+        return output
+
+# TODO: vdsr_k 
+"""
+class vdsr_k(nn.Module):
+    def __init__(self, in_channels, out_channels, num_branch):
+        super(vdsr_k, self).__init__()
+        self.num_branch = num_branch
+
+        self.patch_extraction = B.ConvBlock(in_channels, 64, kernel_size=9, norm_type=None, act_type='relu',valid_padding=False, padding=0)
+        self.mapping = B.ConvBlock(64, 32, kernel_size=1, norm_type=None, act_type='relu',valid_padding=False, padding=0)
+
+        self.reconstruct = nn.ModuleList()
+
+        for _ in range(num_branch):
+            self.reconstruct.append(B.ConvBlock(32, out_channels, kernel_size=5, norm_type=None, act_type=None,valid_padding=False, padding=0))
+
+    def forward(self, x, coeff):
+        x = self.patch_extraction(x)
+        x = self.mapping(x)
+
+        hr_list = []
+        for idx in range(self.num_branch):
+            hr_list.append(self.reconstruct[idx](x).mul(torch.reshape(coeff[idx], (-1, 1, 1, 1))))
+
+        hr = hr_list[0]
+
+        for idx in range(self.num_branch - 1):
+            hr = torch.add(hr, hr_list[idx+1])
+
+        return hr
+"""
+
