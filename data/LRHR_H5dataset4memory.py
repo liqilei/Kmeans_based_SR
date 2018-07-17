Fix bugs in gradient-clipping

data/LRHR_H5dataset4memory.py
@@ -1,8 +1,15 @@
 import os
+import torch.multiprocessing as mp
+try:
+    mp.set_start_method('spawn')
+except RuntimeError:
+    pass
+
 import torch.utils.data as data
 import h5py
 import torch
 import numpy as np
+from mpi4py import MPI
 
 from data import common
 
@@ -23,13 +30,12 @@ class LRHRH5Dataset(data.Dataset):
         self.h5data_coeff = None
 
         # tie with h5 paths
-        self.file_data = h5py.File(opt['dataroot_H5'], 'r')
-        self.file_coeff = h5py.File(opt['coeffroot_H5'], 'r')
+        self.file_data = h5py.File(opt['dataroot_H5'], 'r', swmr=True)
+        # self.file_coeff = h5py.File(opt['coeffroot_H5'], 'r')
 
         # read dataset from h5 files
-        self.h5data_HR = self.file_data['label']
-        self.h5data_LR = self.file_data['data']
-        self.h5data_coeff = [self.file_coeff[k] for k in list(self.file_coeff.keys())]
+        self.h5data_LR = self.file_data.get('data')
+        self.h5data_HR = self.file_data.get('label')
 
         assert self.h5data_HR.shape[0] > 0, 'Error: HR file are empty.'
 
@@ -38,19 +44,8 @@ class LRHRH5Dataset(data.Dataset):
                 'HR and LR datasets have different number of images - {}, {}.'.format(\
                 self.h5data_HR.shape[0], self.h5data_LR.shape[0])
 
-
     def __getitem__(self, index):
-        patch_LR = self.h5data_LR[index]
-        patch_HR = self.h5data_HR[index]
-        coeff = [cof[index, :][0] for cof in self.h5data_coeff]
-        if self.opt['phase'] == 'train':
-            patch_LR, patch_HR = common.h5_augment([patch_LR, patch_HR], self.opt['use_flip'], self.opt['use_rot'])
-
-        tensor_LR = torch.from_numpy(np.ascontiguousarray(patch_LR)).float()
-        tensor_HR = torch.from_numpy(np.ascontiguousarray(patch_HR)).float()
-
-        return {'LR': tensor_LR, 'HR': tensor_HR, 'coeff': coeff}
-
+        return {'LR': torch.from_numpy(self.h5data_LR[index,:,:,:]), 'HR': torch.from_numpy(self.h5data_HR[index,:,:,:])}
 
     def __len__(self):
         return self.h5data_HR.shape[0]

data/__init__.py
@@ -5,14 +5,14 @@ def create_dataloader(dataset, dataset_opt):
     phase = dataset_opt['phase']
     if phase == 'train':
         batch_size = dataset_opt['batch_size']
-        shuffle = True
+        #shuffle = True
         num_workers = dataset_opt['n_workers']
     else:
         batch_size = dataset_opt['batch_size']
-        shuffle = False
+        #shuffle = False
         num_workers = 1
     return torch.utils.data.DataLoader(
-        dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=True)
+        dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
 
 
 def create_dataset(dataset_opt):
@@ -25,6 +25,8 @@ def create_dataset(dataset_opt):
         from data.LRHR_seg_dataset import LRHRSegDataset as D
     elif mode == 'LRHR_H5':
         from data.LRHR_H5dataset import LRHRH5Dataset as D
+    elif mode == 'LRHR_H5_M':
+        from data.LRHR_H5dataset4memory import LRHRH5Dataset as D
     else:
         raise NotImplementedError("Dataset [%s] is not recognized." % mode)
     dataset = D(dataset_opt)

models/SRModel.py
@@ -41,7 +41,7 @@ class SRModel(BaseSolver):
             if loss_type == 'l1':
                 self.criterion_pix = nn.L1Loss()
             elif loss_type == 'l2':
-                self.criterion_pix = nn.MSELoss()
+                self.criterion_pix = nn.MSELoss(size_average=False)
             else:
                 raise NotImplementedError('[ERROR] Loss type [%s] is not implemented!'%loss_type)
 
@@ -50,13 +50,16 @@ class SRModel(BaseSolver):
             self.criterion_pix_weight = self.train_opt['pixel_weight']
 
             weight_decay = self.train_opt['weight_decay_G'] if self.train_opt['weight_decay_G'] else 0
-            optim_params = []
-            for k, v in self.model.named_parameters():
-                if v.requires_grad:
-                    optim_params.append(v)
-                else:
-                    print('[WARNING] Parameters [%s] will not be optimized!'%k)
-                self.optimizer = optim.Adam(optim_params, lr=self.train_opt['lr_G'], weight_decay=weight_decay)
+            # optim_params = []
+            # for k, v in self.model.named_parameters():
+            #     if v.requires_grad:
+            #         optim_params.append(v)
+            #     else:
+            #         print('[WARNING] Parameters [%s] will not be optimized!'%k)
+
+            # self.optimizer = optim.Adam(optim_params, lr=self.train_opt['lr_G'], weight_decay=weight_decay)
+            # self.optimizer = optim.Adam(self.model.parameters(), lr=self.train_opt['lr_G'], weight_decay=weight_decay)
+            self.optimizer = optim.SGD(self.model.parameters(), lr=self.train_opt['lr_G'], momentum=self.train_opt['beta1_G'], weight_decay=weight_decay)
 
             if self.train_opt['lr_scheme'].lower() == 'multisteplr':
                 self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, self.train_opt['lr_steps'], self.train_opt['lr_gamma'])
@@ -95,8 +98,10 @@ class SRModel(BaseSolver):
             self.SR = self.model(self.var_LR, self.coeff)
         else:
             self.SR = self.model(self.var_LR)
-        loss_pix = self.criterion_pix_weight*self.criterion_pix(self.SR, self.var_HR)
+        loss_pix = self.criterion_pix_weight * self.criterion_pix(self.SR, self.var_HR)
         loss_pix.backward()
+        if self.train_opt['clip_grad']:
+            nn.utils.clip_grad_norm_(self.model.parameters(), self.train_opt['clip_grad'])
         self.optimizer.step()
 
         loss_step = loss_pix

models/modules/vdsr.py
@@ -9,12 +9,12 @@ class vdsr(nn.Module):
         super(vdsr, self).__init__()
         self.conv = nn.ModuleList()
 
-        self.conv_in = B.ConvBlock(in_channels, 64, kernel_size=3, norm_type=None, act_type='relu',valid_padding=True)
+        self.conv_in = B.ConvBlock(in_channels, 64, kernel_size=3, norm_type=None, act_type='relu',valid_padding=True, bias=False)
 
         for i in range(18):
-            self.conv.append(B.ConvBlock(64, 64, kernel_size=3, norm_type=None, act_type='relu',valid_padding=True))
+            self.conv.append(B.ConvBlock(64, 64, kernel_size=3, norm_type=None, act_type='relu',valid_padding=True, bias=False))
 
-        self.conv_out = B.ConvBlock(64, out_channels, kernel_size=3, norm_type=None, act_type=None, valid_padding=True)
+        self.conv_out = B.ConvBlock(64, out_channels, kernel_size=3, norm_type=None, act_type=None, valid_padding=True, bias=False)
 
     def forward(self, input):
         x = self.conv_in(input)